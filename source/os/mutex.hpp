89d442ab22f5fbee73187619721e04be082c64d1
Daan Timmer <daan.timmer@gmail.com>
2019-04-16 23:50:49 +0200

    Implemented first version of a mutex

--- source/os/mutex.hpp
+++ source/os/mutex.hpp
@@ -1,6 +1,57 @@
 
 #pragma once
 
+#include "criticalsection.hpp"
+#include "scheduler.hpp"
+
 struct Mutex
 {
+    struct Resource
+    {
+        friend Mutex;
+
+    protected:
+        volatile Mutex* owner = nullptr;
+    };
+
+    Mutex(Mutex::Resource& obj)
+        : obj(obj)
+    {
+    }
+
+    void Acquire()
+    {
+        do
+        {
+            EnterCriticalSection();
+
+            if (obj.owner != this)
+            {
+                obj.owner = this;
+
+				ExitCriticalSection();
+            }
+			else
+			{
+                ExitCriticalSection();
+
+                YieldTask();
+			}
+
+        } while (obj.owner != this);
+    }
+
+    void Release()
+    {
+        EnterCriticalSection();
+
+        obj.owner = nullptr;
+
+        ExitCriticalSection();
+    }
+
+private:
+    Mutex::Resource& obj;
 };
+
+using TheBottleneck = Mutex;
