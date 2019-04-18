c79f9c675f2bdb3221ff99cbe9902ed120508700
Daan Timmer <daan.timmer@gmail.com>
2019-04-18 23:26:57 +0200

    Moved systick setup to own file. Added compile time constants for tickfrequency, tickratio and ticks

---
+++ source/port/stm32f1xx/systemtick.hpp
@@ -0,0 +1,14 @@
+
+#include <chrono>
+#include <ratio>
+
+namespace systemtick
+{
+constexpr uint32_t tickfrequency = 1000;
+
+using tickratio = std::ratio<1, tickfrequency>;
+using ticks = std::chrono::duration<std::int64_t, tickratio>;
+
+void Setup();
+
+} // namespace systemtick
