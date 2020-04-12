c57de5ca34298aee8d4d37e552e197b0bb7771c5
Daan Timmer <daan.timmer@gmail.com>
2020-04-13 00:12:31 +0200

    move schedulerSwitchContextWrapper from static to anonymous namespace

--- src/interrupts.cpp
+++ src/interrupts.cpp
@@ -1,61 +1,63 @@
 
-#include "kernel/InterruptMasking.hpp"
+#include "kernel/interruptMasking.hpp"
 #include "kernel/scheduler.hpp"
 #include "kernel/task.hpp"
 // #include "kernel/utils.hpp"
-#include "stm32f103xb.h"
+#include "stm32f1xx.h"
 #include "stm32f1xx_ll_gpio.h"
-
 #include <algorithm>
 #include <array>
 #include <cassert>
 #include <cstdint>
 
-constexpr std::uint32_t MAIN_RETURN{0xFFFFFFF9ul};   //Tells the handler to return using the MSP
+constexpr std::uint32_t MAIN_RETURN{0xFFFFFFF9ul}; //Tells the handler to return using the MSP
 constexpr std::uint32_t THREAD_RETURN{0xFFFFFFFDul}; //Tells the handler to return using the PSP
 
 constexpr std::uint32_t MAX_SYSCALL_INTERRUPT_PRIORITY = 1;
 
-static void* schedulerSwitchContextWrapper()
+namespace
 {
-    // currentTaskControlBlock->SetStackPointer(ptr);
-    // TaskScheduler();
-    return currentTaskControlBlock->GetStackPointer();
+    auto schedulerSwitchContextWrapper() -> void*
+    {
+        // currentTaskControlBlock->SetStackPointer(ptr);
+        // TaskScheduler();
+        return currentTaskControlBlock->GetStackPointer();
+    }
 }
 
 extern "C"
 {
     void NMI_Handler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void HardFault_Handler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void MemManage_Handler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void BusFault_Handler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void UsageFault_Handler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
@@ -88,7 +90,7 @@ extern "C"
 
     void DebugMon_Handler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
@@ -143,301 +145,301 @@ extern "C"
 
     void WWDG_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void PVD_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void TAMPER_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void RTC_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void FLASH_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void RCC_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void EXTI0_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void EXTI1_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void EXTI2_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void EXTI3_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void EXTI4_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void DMA1_Channel1_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void DMA1_Channel2_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void DMA1_Channel3_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void DMA1_Channel4_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void DMA1_Channel5_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void DMA1_Channel6_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void DMA1_Channel7_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void ADC1_2_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void USB_HP_CAN1_TX_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void USB_LP_CAN1_RX0_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void CAN1_RX1_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void CAN1_SCE_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void EXTI9_5_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void TIM1_BRK_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void TIM1_UP_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void TIM1_TRG_COM_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void TIM1_CC_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void TIM2_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void TIM3_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void TIM4_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void I2C1_EV_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void I2C1_ER_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void I2C2_EV_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void I2C2_ER_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void SPI1_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void SPI2_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void USART1_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void USART2_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void USART3_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void EXTI15_10_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void RTCAlarm_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
     }
     void USBWakeUp_IRQHandler()
     {
-        while (1)
+        while (true)
         {
             assert(0);
         }
