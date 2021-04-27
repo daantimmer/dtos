f1fb1ae25b77f5fe85cafe500aefcdbcf011d005
Daan Timmer <daan.timmer@gmail.com>
2021-04-27 12:24:41 +0200

    disabled custom cmake functions

--- cmake/add_source.cmake
+++ cmake/add_source.cmake
@@ -1,21 +1,21 @@
-function(add_source visibility)
-    cmake_parse_arguments(
-        PRFX
-        ""
-        ""
-        "FW;SRC;TST"
-        ${ARGN}
-    )
+# function(add_source visibility)
+#     cmake_parse_arguments(
+#         PRFX
+#         ""
+#         ""
+#         "FW;SRC;TST"
+#         ${ARGN}
+#     )
     
-    if (PRFX_SRC)
-        target_sources(${TARGET} ${visibility} ${PRFX_SRC})
-    endif()
+#     if (PRFX_SRC)
+#         target_sources(${TARGET} ${visibility} ${PRFX_SRC})
+#     endif()
 
-    if (PRFX_FW AND IS_FIRMWARE)
-        target_sources(${TARGET} ${visibility} ${PRFX_FW})
-    endif()
+#     if (PRFX_FW AND IS_FIRMWARE)
+#         target_sources(${TARGET} ${visibility} ${PRFX_FW})
+#     endif()
 
-    if (PRFX_TST AND IS_TESTING)
-        target_sources(${TARGET} ${visibility} ${PRFX_TST})
-    endif()
-endfunction()
+#     if (PRFX_TST AND IS_TESTING)
+#         target_sources(${TARGET} ${visibility} ${PRFX_TST})
+#     endif()
+# endfunction()
--- cmake/add_target.cmake
+++ cmake/add_target.cmake
@@ -1,96 +1,98 @@
-function(add_target executable)
+# function(add_target executable)
 
-    if (IS_FIRMWARE)
-        set(ELF ${executable}.elf)
-        set(MAP ${executable}.map)
-        set(HEX ${executable}.hex)
-        set(LST ${executable}.lst)
+#     if (IS_FIRMWARE)
+#         set(ELF ${executable}.elf)
+#         set(MAP ${executable}.map)
+#         set(HEX ${executable}.hex)
+#         set(LST ${executable}.lst)
 
-        set(TARGET ${ELF})
-        set(TARGET ${ELF} PARENT_SCOPE)
+#         set(TARGET ${ELF})
+#         set(TARGET ${ELF} PARENT_SCOPE)
 
-        add_executable(${TARGET})
+#         add_executable(${TARGET})
 
-        add_subdirectory(src)
+#         add_subdirectory(src)
         
-        if (MSVC)
-            target_compile_options(${TARGET} PRIVATE /W4 /WX /D_CRT_SECURE_NO_WARNINGS)
-        else()
-            target_compile_options(${TARGET} PRIVATE -Wall -Wextra -Wpedantic -Werror -Wno-unused-parameter)
-        endif()
+#         if (MSVC)
+#             target_compile_options(${TARGET} PRIVATE /W4 /WX /D_CRT_SECURE_NO_WARNINGS)
+#         else()
+#             target_compile_options(${TARGET} PRIVATE -Wall -Wextra -Wpedantic -Werror -Wno-unused-parameter)
+#         endif()
 
-        target_include_directories(${TARGET} PUBLIC src)
+#         target_include_directories(${TARGET} PUBLIC src)
 
-        set_target_properties(
-            ${TARGET}
+#         set_target_properties(
+#             ${TARGET}
 
-            PROPERTIES
-                C_STANDARD 11
-                C_STANDARD_REQUIRED YES
-                C_EXTENSIONS NO
+#             PROPERTIES
+#                 C_STANDARD 11
+#                 C_STANDARD_REQUIRED YES
+#                 C_EXTENSIONS NO
 
-                CXX_STANDARD 17
-                CXX_STANDARD_REQUIRED YES
-                CXX_EXTENSIONS NO
-        )
+#                 CXX_STANDARD 17
+#                 CXX_STANDARD_REQUIRED YES
+#                 CXX_EXTENSIONS NO
+#         )
 
-            target_link_options(${TARGET}
-                PRIVATE
-                    -T${PROJECT_SOURCE_DIR}/lib/target/stm32f103/arm/src/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc/linker/STM32F103XB_FLASH.ld
-                    -mthumb 
-                    -mcpu=cortex-m3 
-                    -ggdb 
-                    -ffunction-sections
-                    -fdata-sections
-                    -specs=nano.specs 
-                    -specs=nosys.specs 
-                    -Wl,--gc-sections 
-                    -Wl,-Map=${executable}.map)
-        target_link_libraries(${TARGET} PUBLIC ${ARGN})
+#         target_link_options(${TARGET}
+#             PRIVATE
+#                 -T${PROJECT_SOURCE_DIR}/lib/target/bluepill/STM32F103C8TX_FLASH.ld
+#                 -mthumb 
+#                 -mcpu=cortex-m3 
+#                 -ggdb 
+#                 -ffunction-sections
+#                 -fdata-sections
+#                 -specs=nano.specs 
+#                 -specs=nosys.specs 
+#                 -Wl,--print-memory-usage
+#                 -Wl,--gc-sections 
+#                 -Wl,-Map=${executable}.map)
+            
+#         target_link_libraries(${TARGET} PUBLIC ${ARGN})
 
 
-        # generate the lst file
-        add_custom_command(
-            OUTPUT ${LST}
+#         # generate the lst file
+#         add_custom_command(
+#             OUTPUT ${LST}
 
-            COMMAND
-                ${CMAKE_OBJDUMP} -h -S ${TARGET} > ${LST}
+#             COMMAND
+#                 ${CMAKE_OBJDUMP} -h -S ${TARGET} > ${LST}
 
-            DEPENDS ${TARGET}
-        )
+#             DEPENDS ${TARGET}
+#         )
 
-        # create hex file
-        add_custom_command(
-            OUTPUT ${HEX}
+#         # create hex file
+#         add_custom_command(
+#             OUTPUT ${HEX}
 
-            COMMAND
-                ${CMAKE_OBJCOPY} -j .text -j .data -O ihex ${TARGET} ${HEX}
+#             COMMAND
+#                 ${CMAKE_OBJCOPY} -j .text -j .data -O ihex ${TARGET} ${HEX}
 
-            DEPENDS ${TARGET}
-        )
+#             DEPENDS ${TARGET}
+#         )
 
-        add_custom_command(
-            OUTPUT "print-size-${TARGET}"
+#         add_custom_command(
+#             OUTPUT "print-size-${TARGET}"
 
-            COMMAND
-                ${ARM_SIZE} ${TARGET}
+#             COMMAND
+#                 ${ARM_SIZE} ${TARGET}
 
-            DEPENDS ${TARGET}
-        )
+#             DEPENDS ${TARGET}
+#         )
 
-        # build the intel hex file for the device
-        add_custom_target(
-            ${executable}
-            ALL
-            DEPENDS ${HEX} ${LST} "print-size-${TARGET}"
-        )
+#         # build the intel hex file for the device
+#         add_custom_target(
+#             ${executable}
+#             ALL
+#             DEPENDS ${HEX} ${LST} "print-size-${TARGET}"
+#         )
 
-        set_target_properties(
-            ${executable}
+#         set_target_properties(
+#             ${executable}
 
-            PROPERTIES
-                OUTPUT_NAME ${TARGET}
-        )
-    endif()
+#             PROPERTIES
+#                 OUTPUT_NAME ${TARGET}
+#         )
+#     endif()
 
-endfunction()
+# endfunction()
