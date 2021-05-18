3f0c9dfbdc533f206d94bffe9da64cd4faa63545
Daan Timmer <daan.timmer@gmail.com>
2021-05-19 00:47:19 +0200

    Applied NOLINT

--- lib/kernel/src/kernel/mainthread.cpp
+++ lib/kernel/src/kernel/mainthread.cpp
@@ -1,11 +1,11 @@
 #include "kernel/mainthread.hpp"
 #include "stm32f1xx.h"
 
-// __processStack_end is provided by the linker script and does not map 1:1 to std::array therefor block
-// cppcoreguidelines-avoid-c-arrays
-// NOLINTNEXTLINE
-extern std::uint32_t __processStack_start[];
-extern std::uint32_t __processStack_end[];
+// __processStack_start and _end is provided by the linker script
+// and does not map 1:1 to std::array therefor
+// block modernize-avoid-c-arrays and cppcoreguidelines-avoid-c-arrays
+extern std::uint32_t __processStack_start[]; // NOLINT(modernize-avoid-c-arrays, cppcoreguidelines-avoid-c-arrays)
+extern std::uint32_t __processStack_end[]; // NOLINT(modernize-avoid-c-arrays, cppcoreguidelines-avoid-c-arrays)
 
 kernel::MainThread::MainThread()
     : TaskBase{{&__processStack_end[0], static_cast<std::size_t>(&__processStack_end[0] - &__processStack_start[0])},
